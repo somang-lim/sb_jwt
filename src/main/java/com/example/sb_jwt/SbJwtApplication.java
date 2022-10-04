2f3bd27cd21c718f59419008ec8b5a6e99b576d4
somang-lim <somangjuha@gmail.com>
2022-10-04 13:49:31 +0900

    세팅

---
+++ src/main/java/com/example/sb_jwt/SbJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sb_jwt/SbJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
